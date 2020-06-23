55cac2061699e72b5795bc966278687838c91251
Luis Felipe Cordeiro Sena <61807189+luisfelipesena@users.noreply.github.com>
2020-06-23 13:22:36 -0300

    Create antecessor_e_sucessor.c

---
+++ antecessor_e_sucessor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    
+        printf("%d",n-1);
+        printf(" ");
+        printf("%d",n+1);
+   
+	return 0;
+}
